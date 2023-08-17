2ba53950b14a34d31c137d32d9d9bfc182470a72
akshay4884 <89313089+akshay4884@users.noreply.github.com>
2023-08-17 08:28:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,7 +63,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔅[{get_size(file.file_size)}] 💮 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🔅[{get_size(file.file_size)}] ⚜️ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -649,7 +649,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔅[{get_size(file.file_size)}] 💮 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"🔅[{get_size(file.file_size)}] ⚜️ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
